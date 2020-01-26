ca73a243d7476402faadacc6883a115d61cbc08f
stone <vannakseng1996@gmail.com>
2020-01-26 14:09:27 +0700

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'ng-read-excel';
+  result: any = {};
+  sheets = [];
+  sheet = '';
+  constructor() {
+
+  }
+
+  fileChangeEvent(evt) {
+    this.sheets = [];
+    this.result = {};
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+
+      /* grab first sheet */
+      // const wsname: string = wb.SheetNames[0];
+      wb.SheetNames.forEach(element => {
+        this.sheets.push(element);
+        const ws: XLSX.WorkSheet = wb.Sheets[element];
+        this.result[element] = <any>(XLSX.utils.sheet_to_json(ws, {header: 1}));
+      });
+      this.sheet = wb.SheetNames[0];
+      // const ws: XLSX.WorkSheet = wb.Sheets[this.sheets[0]];
+      // /* save data */
+      // this.result = <any>(XLSX.utils.sheet_to_json(ws, {header: 1}));
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+
+  onClick(sheet: string) {
+    this.sheet = sheet;
+  }
 }
